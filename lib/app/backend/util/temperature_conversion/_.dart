72c708652235ab5cf6104d3a00a7d10b2d09bd26
june lee <melodysdreamj@gmail.com>
2024-08-04 15:37:58 +0900

    `

---
+++ lib/app/backend/util/temperature_conversion/_.dart
@@ -0,0 +1,9 @@
+// 화씨를 섭씨로 변환하는 함수
+double fahrenheitToCelsius(double fahrenheit) {
+  return (fahrenheit - 32) * 5 / 9;
+}
+
+// 섭씨를 화씨로 변환하는 함수
+double celsiusToFahrenheit(double celsius) {
+  return (celsius * 9 / 5) + 32;
+}
